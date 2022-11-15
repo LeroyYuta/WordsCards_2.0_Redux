fb46ec6c1a046226582326022aac427b270189f2
LeroyYuta <ljuy1999@mail.ru>
2022-11-15 20:03:44 +0500

    update card comp

--- src/Pages/CardsPage/CardsPage.jsx
+++ src/Pages/CardsPage/CardsPage.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
-import { Card } from '../index';
+import React, { useState } from 'react';
+import Button from '../components/Button/Button';
+import { next, prev } from '../index';
+
+const initialCount = {
+    count: 0,
+    checks: 0
+}
 
 const CardsPage = ({
     words
 }) => {
+    const [count, setCount] = useState(initialCount);
+    const [isPressed, setIsPressed] = useState({});
+
+    const handleClickNext = () => {
+        setCount({
+            ...count,
+            count: count.count + 1
+        });
+    }
+
+    const handleClickPrev = () => {
+        setCount({
+            ...count,
+            count: count.count - 1
+        });
+    }
+
+    const handleClickCheck = id => {
+        setIsPressed({
+            ...isPressed,
+            [id]: true,
+        });
+        setCount({
+            ...count,
+            checks: count.checks + 1
+        });
+    }
+
+    const wordToUpperCaseEn = words[count.count].english.toUpperCase(),
+        wordToUpperCaseRu = words[count.count].russian.toUpperCase();
+
+    const showBtnCheck = isPressed[words[count.count].id] ? 'disabled' : 'check',
+        showWordRu = isPressed[words[count.count].id] ? 'word-ru' : 'disabled';
+
+    const prevClass = count.count === 0 ? 'disabled' : 'prev arrow',
+        nextClass = count.count === words.length - 1 ? 'disabled' : 'next arrow';
+
     return (
-        <Card words={words} />
+        <div className='card-block'>
+            <div className='learned'>
+                <p>Learned: {count.checks}/{words.length}</p>
+            </div>
+            <div className='cards'>
+                <img className={prevClass} src={prev} alt='prev' onClick={handleClickPrev} />
+                <div className='card'>
+                    <div className='words'>
+                        <p className="word-en">{wordToUpperCaseEn}</p>
+                        <p className={showWordRu}>{wordToUpperCaseRu}</p>
+                    </div>
+                    <div className="choose-btn">
+                        <Button className={showBtnCheck} label='CHECK' onClick={() => handleClickCheck(words[count.count].id)} />
+                    </div>
+                </div >
+                <img className={nextClass} src={next} alt='next' onClick={handleClickNext} />
+            </div>
+            <div className='count'>
+                <p>{count.count + 1}/{words.length}</p>
+            </div>
+        </div>
     )
 }
 
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,7 +3,6 @@ import HomePage from './HomePage/HomePage';
 import TablePage from './TablePage/TablePage';
 import CardsPage from './CardsPage/CardsPage';
 import Button from './components/Button/Button';
-import Card from './components/Card/Card';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Modal from './components/Modal/Modal';
@@ -14,4 +13,4 @@ import prev from '../assets/img/prev.png';
 import logo from '../assets/img/logo.png';
 import robot404 from '../assets/img/404.png';
 
-export {HomePage, TablePage, CardsPage, GamePage, Button, Card, Header, Footer, Modal, next, prev, logo, robot404}
+export {HomePage, TablePage, CardsPage, GamePage, Button, Header, Footer, Modal, next, prev, logo, robot404}
